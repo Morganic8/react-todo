a65f83c4630a5316d075117d609c1246913fc10f
Morgan Atwood <atwood.morganj@gmail.com>
2017-11-10 11:21:32 -0800

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -32,4 +32,49 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+
+  describe('todosReducers', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      }
+
+
+      var res = reducers.todosReducers(df([]), df(action));
+
+
+      //check if item added to array
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+
+    });
+
+
+    it('should toggle todo', () => {
+      var todos = [{
+        id: '123',
+        text: 'Something',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+
+
+      var res = reducers.todosReducers(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
+
+
+
 });
